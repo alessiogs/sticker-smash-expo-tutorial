458d4bcc5aeb7c4b2a611da9d2b2b1a511a9fc6d
alessiogs <alessio.soggiu@hotmail.it>
2026-02-03 22:50:53 +0100

    feat: home screen introduced

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,31 @@
+import { Link } from "expo-router"
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+export default function about() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About Screen</Text>
+      <Link href="/" style={styles.button}>
+        Go to Home screen
+      </Link>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+})
